31c302e9cbb14a29c7a907d0107cb4ccd3bb10de
Richard van Zon <richard@relectus.com>
2023-01-16 15:50:57 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <img src="https://valuemetrics.nl/wp-content/uploads/2020/02/cropped-200224_Website_ValueMetrics-Logo-RGB-3.png" />
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<script setup lang="ts">
+  import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<style scoped>
+  .home {
+    background-color: white;
+  }
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello-world">
+    <h1>{{ msg }}</h1>
+    <button @click="loadQuestions">Load</button>
+    <div>{{ questions }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { ref } from "vue";
+
+  defineProps<{ msg: string }>();
+
+  const count = ref(0);
+  const questions = ref([]);
+
+  const loadQuestions = async () => {
+    const result = await fetch("https://opentdb.com/api.php?amount=10");
+    questions.value = result.text;
+    console.log("result:", result);
+  };
+</script>
+
+<style scoped>
+  h1 {
+    margin-top: 100px;
+    background-color: red;
+  }
+</style>
